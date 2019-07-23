09e72c0da89181fe74d7c0945e04954dcf0c9050
Flora <caponi.flora@gmail.com>
2019-07-23 21:28:36 +0200

    update

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,9 @@
 require 'bundler'
-Bundler.require
+Bundler.require #permet d'appeler les gems contenues dans le Gemfiles
 
 require_relative 'player.rb'
+#permet de lier les fichiers game et player pôur utiliser les classes et méthodes qu'ils contiennent
+
 
 @@all_enemies = []
 
@@ -9,20 +11,21 @@ class Game
 	attr_accessor :human_player, :enemies
 
 	def initialize(title)
-		player1.to_s = Player.new("Yvon") 
-		player2.to_s = Player.new("José") 
-		player3.to_s = Player.new("Jean") 
-		player4.to_s = Player.new("Marie")
-		@@all_enemies << self
+		player1 = Player.new("Yvon") 
+		player2 = Player.new("José") 
+		player3 = Player.new("Jean") 
+		player4 = Player.new("Marie") #on initialise quatres instances de la classe Player
+		@@all_enemies << self #on les met dans un array
 		@human_player = HumanPlayer.new("#{title}")
 	end 
 
 	def self.kill_player(player_killed)
-		@@all_enemies.delete(player_killed)
+		@@all_enemies.delete(@@all_enemies.index(player_killed.name)) #supprime l'ennemi choisi de la liste des ennemis, cela ne fonctionne pas
 	end 
 
-	def is_still_ongoing
-		if @human_player.life_points > 0 && @enemies != nil
+	def is_still_ongoing 
+		if @human_player.life_points > 0 && @enemies != nil 
+		#vérifie si le jeu est toujours en cours (le joueur a toujours des Pv et tous les ennemis ne sont pas morts)
 			return true 
 		else return false
 		end
